637b4b0ba3e4e4f8c961525e9ef0c9eaa70e92d2
Prerona Mukherjee <prerona24@gmail.com>
2020-10-21 22:22:45 -0500

    5

--- config/orm.js
+++ config/orm.js
@@ -1,42 +1,100 @@
-const connection = require("../config/connection.js");
+// Import (require) connection.js
+var connection = require("../config/connection.js");
 
-const orm = {
-  selectAll: function (tableName, cb) {
-    const queryString = `SELECT * FROM ${tableName};`;
-    connection.query(queryString, function (err, result) {
-      if (err) {
-        throw err;
-      }
-      cb(result);
-    });
-  },
-  insertOne: function (tableName, colName, val, cb) {
-    const querySting = `INSERT INTO ${tableName} (${colName}) VALUES ("${val}")`;
-    connection.query(querySting, function (err, result) {
-      if (err) {
-        throw err
-      };
-      cb(result);
-    });
-  },
-  updateOne: function (tableName, colName, boolean, condition, cb) {
-    const querySting = `UPDATE ${tableName} SET ${colName} = ${boolean} WHERE ${condition}`;
-    connection.query(querySting, function (err, result) {
-      if (err) {
-        throw err
-      };
-      cb(result);
-    });
-  },
-  deleteOne: function (tableName, condition, cb) {
-    const queryString = `DELETE FROM ${tableName} WHERE ${condition}`;
-    connection.query(queryString, function (err, result) {
-      if (err) {
-        throw err
-      };
-      cb(result);
-    });
-  }
+// Helper function for SQL syntax.
+function printQuestionMarks(num) {
+    var arr = [];
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+        var value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+            // e.g. {sleepy: true} => ["sleepy=true"]
+            arr.push(key + "=" + value);
+        }
+    }
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+}
+
+var orm = {
+    // Display all burgers in the db.
+    selectAll: function(table, cb) {
+        var queryString = "SELECT * FROM " + table + ";";
+
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    // Add a burger to the db.
+    insertOne: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+                throw err
+            }
+            cb(result);
+        });
+    },
+    // Set burger devoured status to true.
+    updateOne: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err
+            }
+            cb(result);
+        });
+    },
+    // Delete a burger from the db.
+    deleteOne: function(table, condition, cb) {
+        var queryString = "DELETE FROM " + table;
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err
+            }
+            cb(result);
+        });
+    }
 };
 
+// Export the ORM object in module.exports.
 module.exports = orm;
